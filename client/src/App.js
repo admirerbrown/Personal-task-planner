6c276de9dc23d94c48eea90b2c125ec1a28c1632
admirerbrown <admirerbrown@gmail.com>
2023-09-06 22:45:48 +0000

    task are group according to status on taskboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import HomePage  from './components/Home'
+import NavBar from './components/Navigation'
+import { Routes, Route } from 'react-router-dom';
+import TaskBoard from './components/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
+    <div className="App flex">
+      <NavBar></NavBar>
+      <div className="viewer min-h-screen border w-full text-white border-blue-400 bg-purple-600">
+        <Routes>
+          <Route path="/taskboard" element={<TaskBoard></TaskBoard>} />
+        </Routes>
+      </div>
     </div>
   );
 }

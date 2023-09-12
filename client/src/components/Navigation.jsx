a92db2ce3cba8fbaaf82b1ad53e1b65539f3cb12
admirerbrown <admirerbrown@gmail.com>
2023-09-12 22:48:27 +0000

    clean ui navbar phase1 done

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -4,54 +4,99 @@ import { GoGoal } from "react-icons/go";
 import { RiDashboard3Line } from "react-icons/ri";
 import { TbLayoutKanban } from "react-icons/tb";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <div className="flex flex-col items-start pl-5 panel min-h-screen w-72 text-white border border-orange-400 bg-red-950">
-      <div className="flex items-center w-full">
-        <div className="logo h-20 rounded border object-cover mt-2 flex">
-          <img className="h-full w-full" src={logo} alt="achiever logo" />
-        </div>
-        <h2>comer</h2>
-      </div>
+  const location = useLocation();
 
+  return (
+    <div className="flex flex-col items-start pl-5 panel min-h-screen w-72 border shadow-sm bg-[rgb(255,255,255)] font-IBM-Plex-Sans">
+      <div className="flex items-center w-full flex-col gap-5 mb-5">
+        <div className="logo h-20 w-20 object-contain mt-12 shadow-md">
+          <img
+            className="h-full w-full rounded-full"
+            src={logo}
+            alt="achiever logo"
+          />
+        </div>
+        <h2 className="text-lg font-bold capitalize text-[#3c80fd]">comer</h2>
+      </div>
       <ul className="mt-5">
-        <div className="">
-          <h5 className="text-yellow-400 ">HOME</h5>
-          <li className="flex items-center gap-2">
-            <span>
-              <RiDashboard3Line></RiDashboard3Line>
-            </span>
-            <Link to="/dashboard">Dashboard</Link>
+        <div className="flex flex-col gap-4">
+          <h5 className="text-[#a5a9b4] tracking-widest text-md">HOME</h5>
+          <li className="flex items-center text-sm gap-2 font-semibold h-10 w-40 rounded p-1 relative hover:bg-[#3c80fd] hover:text-white">
+            <NavLink
+              to="/dashboard"
+              activeClassName="active-link"
+              className="flex items-center gap-2 "
+            >
+              <RiDashboard3Line />
+              Dashboard
+            </NavLink>
+            {location.pathname === "/dashboard" && (
+              <div className="w-2 h-2 ml-7 bg-green-500 rounded-full absolute top-1/2 right-[-30px] transform -translate-y-1/2"></div>
+            )}
           </li>
         </div>
       </ul>
-      <ul className="mt-5 flex flex-col">
-        <h5 className="text-yellow-400">ACTIVITIES</h5>
-        <li className="flex items-center gap-2">
-          <span>
+      <br />
+
+      <ul className="mt-5 flex flex-col gap-4">
+        <h5 className="text-[#a5a9b4] tracking-widest text-md">ACTIVITIES</h5>
+        <li className="flex items-center text-sm gap-2 font-semibold h-10 w-40 rounded p-1 relative hover:bg-[#3c80fd] hover:text-white">
+          <NavLink
+            to="/taskboard"
+            activeClassName="active-link"
+            className="flex items-center gap-2 "
+          >
             <TbLayoutKanban></TbLayoutKanban>
-          </span>
-          <Link to="/taskboard">Task Board</Link>
+            Task Board
+          </NavLink>
+          {location.pathname === "/taskboard" && (
+            <div className="w-2 h-2 ml-7 bg-green-500 rounded-full absolute top-1/2 right-[-30px] transform -translate-y-1/2"></div>
+          )}
         </li>
-        <li className="mt-3 flex items-center gap-2">
-          <span>
+
+        <li className="flex items-center text-sm gap-2 font-semibold h-10 w-40 rounded p-1 relative hover:bg-[#3c80fd] hover:text-white">
+          <NavLink
+            to="/goals"
+            activeClassName="active-link"
+            className="flex items-center gap-2 "
+          >
             <GoTasklist></GoTasklist>
-          </span>
-          <Link to="/goals">Goals</Link>
+            Goals
+          </NavLink>
+          {location.pathname === "/goals" && (
+            <div className="w-2 h-2 ml-7 bg-green-500 rounded-full absolute top-1/2 right-[-30px] transform -translate-y-1/2"></div>
+          )}
         </li>
-        <li className="mt-3 flex items-center gap-2">
-          <span>
+
+        <li className="flex items-center text-sm gap-2 font-semibold h-10 w-40 rounded p-1 relative hover:bg-[#3c80fd] hover:text-white">
+          <NavLink
+            to="/goalgroup"
+            activeClassName="active-link"
+            className="flex items-center gap-2 "
+          >
             <GoGoal></GoGoal>
-          </span>
-          <Link to="/goalgroup">Goal Category</Link>
+            Goal Category
+          </NavLink>
+          {location.pathname === "/goalgroup" && (
+            <div className="w-2 h-2 ml-7 bg-green-500 rounded-full absolute top-1/2 right-[-30px] transform -translate-y-1/2"></div>
+          )}
         </li>
-        <li className="mt-3 flex items-center gap-2">
-          <span>
+
+        <li className="flex items-center  gap-2 font-semibold h-10 w-40 rounded p-1 relative hover:bg-[#3c80fd] hover:text-white">
+          <NavLink
+            to="/calender"
+            activeClassName="active-link"
+            className="flex items-center gap-2 "
+          >
             <FaRegCalendarAlt></FaRegCalendarAlt>
-          </span>
-          <Link to="/calender">Calender</Link>
+            Calender
+          </NavLink>
+          {location.pathname === "/calender" && (
+            <div className="w-2 h-2 ml-7 bg-green-500 rounded-full absolute top-1/2 right-[-30px] transform -translate-y-1/2"></div>
+          )}
         </li>
       </ul>
     </div>
@@ -59,3 +104,6 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+
+// TODO: MAKE NAVBAR RESPONSIVE ON MEDIA SCREENS FROM LARGE

bc8033d28a7f6675ec8e37abfd6b884609d285a1
admirerbrown <admirerbrown@gmail.com>
2023-09-13 23:40:01 +0000

    card design almost done

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -6,27 +6,42 @@ const TaskBoard = () => {
     {
       _id: 1,
       title: "complete the frontend architecture of the app",
+      summary:
+        "after choosing ui break the frontend tasks in features and milestones ",
       status: "not-started",
+      goal_tag: "job search",
     },
     {
       _id: 2,
       title: "complete the backend architecture of the app",
+      summary:
+        "build the full backend using node.js and express ensure to take care of CORS",
       status: "in-review",
+      goal_tag: "job search",
     },
     {
       _id: 3,
       title: "implement the UI/UX of the app",
+      summary:
+        "go through a list of task management ui's from dribble and choose an attractive design ",
       status: "in-progress",
+      goal_tag: "freelance",
     },
     {
       _id: 4,
       title: "connect the frontend to the backend",
+      summary:
+        "make this fullstack app by ensuring proper communication between the frontend and backend.use axios for the frontend communication and also use redux for state management",
       status: "not-started",
+      goal_tag: "job search",
     },
     {
       _id: 5,
       title: "Ask for code review from the community",
+      summary:
+        "seek code review from the community explaining the main features of the app and what could be improved or eliminated ",
       status: "completed",
+      goal_tag: "freelance",
     },
   ]);
 
@@ -52,30 +67,41 @@ const TaskBoard = () => {
 
   const renderColumn = (status) => (
     <div
-      className={`${status} flex flex-col min-h-screen border mx-5 font-IBM-Plex-Sans text-black`}
+      className={`${status} flex flex-col min-h-screen mx-5 font-IBM-Plex-Sans text-black`}
       onDragOver={onDragOver}
       onDrop={(e) => onDrop(e, status)}
     >
       <div className=" flex items-center justify-between  border-b-2   h-14">
         <div className="flex gap-3 items-center">
           <h2 className="uppercase text-center font-medium">{status}</h2>
-          <div className="h-5 w-5 bg-[#E0EAF3] border rounded justify-center flex text-sm">3</div>
+          <div className="h-5 w-5 bg-[#E0EAF3] border rounded justify-center flex text-sm">
+            4
+          </div>
         </div>
         <div className=" border-2 rounded-md border-dashed h-6 w-6 border-[#c9d8e3] flex justify-center items-center">
-        <IoMdAdd className="text-sm"></IoMdAdd>
-
+          <IoMdAdd className="text-sm"></IoMdAdd>
         </div>
       </div>
       {tasks
         .filter((task) => task.status === status)
         .map((todo) => (
           <div
-            className="card border flex flex-col mt-2 cursor-move"
+            className="card bg-base-100 shadow-md rounded-md cursor-move mt-4"
             draggable
             onDragStart={(e) => onDragStart(e, todo)}
             key={todo._id}
           >
-            <p>{todo.title}</p>
+            <div className="card-body">
+              <h2 className="card-title capitalize text-sm ">{todo.title}</h2>
+              <p className="text-xs text-gray-400">
+                {todo.summary}
+              </p>
+              <div className="card-actions justify-start">
+                <button className="btn-sm rounded-md btn-primary text-xs capitalize">
+                  {todo.goal_tag}
+                </button>
+              </div>
+            </div>
           </div>
         ))}
     </div>

1959a73c120f26d1f9a2f1f3e0761a63b366c355
admirerbrown <admirerbrown@gmail.com>
2023-09-22 23:54:02 +0000

    showing list of goals

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import NavBar from './components/Navigation'
+import NavBar from './components/Navigation';
 import { Routes, Route } from 'react-router-dom';
 import TaskBoard from './components/Tasks';
+import ShowGoals from './components/DisplayGoals';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="viewer min-h-screen w-full text-white  bg-[rgb(244,248,251)]">
         <Routes>
           <Route path="/taskboard" element={<TaskBoard></TaskBoard>} />
+          <Route path="/goals" element={<ShowGoals></ShowGoals>} />
         </Routes>
       </div>
     </div>
---
+++ client/src/db/goalData.js
@@ -0,0 +1,83 @@
+export const goalList = [
+    {
+        _id: 1,
+        title: "attend 10 mock interviews on pramp per month",
+        description:
+            "get familiar with answering common behavioral and technical interview question through peer mock interview on pramp",
+        status: "Planned",
+        category: "Interview Prep",
+    },
+    {
+        _id: 2,
+        title: "Advanced DSA Proficiency",
+        description: "Master advanced data structures and algorithms to excel in technical interviews by the end of 2023.",
+        status: "Ongoing",
+        category: "Interview Prep",
+    },
+    // {
+    //     _id: 3,
+    //     title: "Expand Professional Network",
+    //     description: "Connect with at least 100 professionals in your field to expand your network and opportunities.",
+    //     status: "Ongoing",
+    //     category: "Networking",
+    // },
+    // {
+    //     _id: 4,
+    //     title: "Secure Full-time Software Developer Role",
+    //     description: "Find and secure a full-time software developer position in a reputable company by mid-2023.",
+    //     status: "Ongoing",
+    //     category: "Job Search & Projects",
+    // },
+    // {
+    //     _id: 5,
+    //     title: "Build a Freelance Portfolio",
+    //     description: "Complete 5 freelance projects to build a strong portfolio for freelance work by the end of 2023.",
+    //     status: "Planned",
+    //     category: "Freelance Portfolio Buildup",
+    // },
+    // {
+    //     _id: 6,
+    //     title: "Participate in 10 Hackathons",
+    //     description: "Engage in 10 hackathons to hone your problem-solving skills and build innovative projects.",
+    //     status: "Ongoing",
+    //     category: "Hackathons",
+    // },
+    // {
+    //     _id: 7,
+    //     title: "System Design Mastery",
+    //     description: "Become proficient in system design concepts and excel in system design interviews by the end of 2023.",
+    //     status: "Planned",
+    //     category: "Interview Prep",
+    // },
+    // {
+    //     _id: 8,
+    //     title: "Attend 5 Industry Conferences",
+    //     description: "Attend five industry conferences to stay updated with the latest trends and network with industry leaders.",
+    //     status: "Planned",
+    //     category: "Networking",
+    // },
+    // {
+    //     _id: 9,
+    //     title: "Complete 2 Personal Projects",
+    //     description: "Work on and complete two personal coding projects to showcase your skills to potential employers.",
+    //     status: "Ongoing",
+    //     category: "Job Search & Projects",
+    // },
+    // {
+    //     _id: 10,
+    //     title: "Secure First Freelance Client",
+    //     description: "Secure your first paying freelance client and deliver a successful project by the end of 2023.",
+    //     status: "Planned",
+    //     category: "Freelance Portfolio Buildup",
+    // },
+    // {
+    //     _id: 11,
+    //     title: "Organize a Local Hackathon",
+    //     description: "Organize a local hackathon event to promote coding skills in your community by the end of 2023.",
+    //     status: "Planned",
+    //     category: "Hackathons",
+    // },
+]
+
+
+

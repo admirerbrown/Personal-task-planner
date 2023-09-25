97b1f264d61aa1b25105bb9ff6c57fa9cb9b5c06
admirerbrown <admirerbrown@gmail.com>
2023-09-25 23:54:47 +0000

    goal list ui almost done

--- client/src/components/DisplayGoals.jsx
+++ client/src/components/DisplayGoals.jsx
@@ -1,7 +1,8 @@
 import { goalList } from "../db/goalData.js";
+
 const ShowGoals = () => {
   return (
-    <div>
+    <div className="scroll-smooth overflow-auto h-screen">
       <div className="m-10">
         <div className="banner-img w-full h-52 bg-slate-600 mb-7 rounded-lg object-cover">
           <img
@@ -21,13 +22,20 @@ const ShowGoals = () => {
 const ListOfGoals = () => {
   const renderListItems = (header) => {
     return (
-      <div className="text-center grow">
-        <h2 className="mb-4 uppercase">{header}</h2>
+      <div className="text-center grow font-IBM-Plex-Sans text-[#394f72] font-semibold">
+        <h2 className="mb-4 uppercase text-left pl-3">{header}</h2>
         <ul className="flex flex-col gap-3 w-full">
           {goalList.map((item, index) => (
             <li
               key={index}
-              className="flex border w-full justify-start items-center p-3 shadow-sm bg-white rounded-sm"
+              // className={`flex border w-full justify-start items-center p-3  shadow-sm bg-white rounded-sm ${header === 'title' ? 'text-[#69bfd1] font-bold' : ''}`}
+              className={`
+  flex border w-full justify-start items-center p-3 shadow-sm bg-white rounded-sm 
+  ${header === 'title' ? 'text-[#69bfd1] font-bold' : ''}
+  ${item.status === "Planned" && header==='status'? "bg-[#fff6f4] text-[#fc8667]" : ""}
+  ${item.status === "Ongoing" && header==='progress'? "bg-[yellow] text-orange-700" : ""}
+  ${item.status === "completed" && header==='progress'? "bg-green-400 text-blue-200" : ""}
+`}
             >
               {item[header]}
             </li>
--- client/src/db/goalData.js
+++ client/src/db/goalData.js
@@ -26,9 +26,9 @@ export const goalList = [
         description: "Connect with at least 100 professionals in your field to expand your network and opportunities.",
         status: "Ongoing",
         category: "Networking",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 4,
@@ -36,9 +36,9 @@ export const goalList = [
         description: "Find and secure a full-time software developer position in a reputable company by mid-2023.",
         status: "Ongoing",
         category: "Job Search & Projects",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 5,
@@ -46,9 +46,9 @@ export const goalList = [
         description: "Complete 5 freelance projects to build a strong portfolio for freelance work by the end of 2023.",
         status: "Planned",
         category: "Freelance Portfolio Buildup",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 6,
@@ -56,9 +56,9 @@ export const goalList = [
         description: "Engage in 10 hackathons to hone your problem-solving skills and build innovative projects.",
         status: "Ongoing",
         category: "Hackathons",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 7,
@@ -66,19 +66,19 @@ export const goalList = [
         description: "Become proficient in system design concepts and excel in system design interviews by the end of 2023.",
         status: "Planned",
         category: "Interview Prep",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 8,
         title: "Attend 5 Industry Conferences",
         description: "Attend five industry conferences to stay updated with the latest trends and network with industry leaders.",
-        status: "Planned",
+        status: "completed",
         category: "Networking",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 9,
@@ -86,9 +86,9 @@ export const goalList = [
         description: "Work on and complete two personal coding projects to showcase your skills to potential employers.",
         status: "Ongoing",
         category: "Job Search & Projects",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 10,
@@ -96,9 +96,9 @@ export const goalList = [
         description: "Secure your first paying freelance client and deliver a successful project by the end of 2023.",
         status: "Planned",
         category: "Freelance Portfolio Buildup",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
     {
         _id: 11,
@@ -106,9 +106,9 @@ export const goalList = [
         description: "Organize a local hackathon event to promote coding skills in your community by the end of 2023.",
         status: "Planned",
         category: "Hackathons",
-    progress: 40,
-    tasks: 5,
-    action:"Delete"
+        progress: 40,
+        tasks: 5,
+        action: "Delete"
     },
 ]
 

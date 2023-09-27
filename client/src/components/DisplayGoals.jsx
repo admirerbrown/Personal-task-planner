615654f327e0ff3172fa8e9985c9dbef2e5ef77f
admirerbrown <admirerbrown@gmail.com>
2023-09-27 23:51:07 +0000

    create goals filter feature

--- client/src/components/DisplayGoals.jsx
+++ client/src/components/DisplayGoals.jsx
@@ -1,6 +1,30 @@
 import { goalList } from "../db/goalData.js";
+import { IoMdAdd } from "react-icons/io";
+import { RiEqualizerFill } from "react-icons/ri";
+import React, { useState } from "react";
 
 const ShowGoals = () => {
+  const [selectedFilter, setSelectedFilter] = useState("all");
+
+  const filteredGoals = goalList.filter((goal) => {
+    if (selectedFilter === "all") {
+      return true;
+    }
+    return goal.status.toLowerCase() === selectedFilter.toLowerCase();
+  });
+
+  const filterOptions = [
+    { value: "all", label: "All" },
+    { value: "most_recent", label: "Most Recent" },
+    { value: "ongoing", label: "Ongoing" },
+    { value: "planned", label: "Planned" },
+    { value: "completed", label: "Completed" },
+  ];
+
+  const handleFilterChange = (event) => {
+    setSelectedFilter(event.target.value);
+  };
+
   return (
     <div className="scroll-smooth overflow-auto h-screen">
       <div className="m-10">
@@ -12,30 +36,64 @@ const ShowGoals = () => {
           />
         </div>
       </div>
+      <div className="capitalize font-IBM-Plex-Sans flex gap-5 ml-10 mb-5 text-xl items-center">
+        <div className="dropdown dropdown-bottom">
+          <label
+            tabIndex={0}
+            className="btn m-1 btn-sm flex gap-3 text-[#394f72] rounded-md border-2 bg-[rgb(244,248,251)] hover:bg-transparent"
+          >
+            <RiEqualizerFill></RiEqualizerFill>Filter
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 text-[#394f72] uppercase"
+          >
+            {filterOptions.map((option) => (
+              <li
+                key={option.value}
+                className={`hover:bg-[rgb(244,248,251)] ${selectedFilter === option.value ? "bg-[rgb(244,248,251)]" : ""
+                  }`}
+              >
+                <a
+                  href="#home"
+                  onClick={() =>
+                    handleFilterChange({ target: { value: option.value } })
+                  }
+                >
+                  {option.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button className="btn btn-sm  rounded-md px-4 text-white flex gap-3 bg-[#3c80fd] hover:bg-[#3c80fd] hover:border-none">
+          <IoMdAdd></IoMdAdd>Add Goal
+        </button>
+      </div>
+
       <div>
-        <ListOfGoals></ListOfGoals>
+        <ListOfGoals goals={filteredGoals}></ListOfGoals>
       </div>
     </div>
   );
 };
 
-const ListOfGoals = () => {
+const ListOfGoals = ({ goals }) => {
   const renderListItems = (header) => {
     return (
       <div className="text-center grow font-IBM-Plex-Sans text-[#394f72] text-sm font-semibold">
         <h2 className="mb-4 uppercase text-left pl-3">{header}</h2>
         <ul className="flex flex-col gap-3 w-full">
-          {goalList.map((item, index) => (
+          {goals.map((item, index) => (
             <li
               key={index}
-              className={`
-                flex border w-full justify-start items-center p-3 shadow-sm bg-white rounded-sm 
-                ${header === 'title' ? 'text-[#69bfd1] font-bold lowercase' : 'uppercase'}
-                ${header === 'action' ? 'text-red-500 font-bold' : 'uppercase'}
-                ${item.status === "Planned" && header==='status'? 'bg-red-100 text-[#fc8667]' : ""}
-                ${item.status === "Ongoing" && header==='status'? 'bg-blue-100 text-[#4e7afc]' : ""}
-                ${item.status === "completed" && header==='status'? 'bg-green-100 text-[#56d428]': ""}
-                ${item.status === "completed" && header==='title'? 'line-through text-[#9aa6af]': ""}
+              className={`flex border w-full justify-start items-center p-3 shadow-sm bg-white rounded-sm ${header === "title" ? "text-[#69bfd1] font-bold lowercase" : "uppercase"}
+                ${header === "action" ? "text-red-500 font-bold" : "uppercase"}
+                ${item.status === "Planned" && header === "status" ? "bg-[#fff6f4] text-[#fc8667]" : ""}
+                ${item.status === "Ongoing" && header === "status" ? "bg-[#d3deff] text-[#4e7afc]" : ""}
+                ${item.status === "completed" && header === "status" ? "bg-[#f0fcec] text-[#56d428]" : ""}
+                ${item.status === "completed" && header === "title" ? "line-through text-[#9aa6af]" : ""}
               `}
             >
               {item[header]}
@@ -63,3 +121,6 @@ const ListOfGoals = () => {
 };
 
 export default ShowGoals;
+
+//TODO: CHANGE THE NAME SHOW-GOALS TO RENDER-GOALS-PAGE
+//TODO: MAKE THE ADD GOAL A MODAL
